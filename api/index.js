260b22407537255a1af6d4fde01263f8ff74e459
shakeelkhan234 <shakeelkhan187004@gmail.com>
2025-01-27 19:38:23 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,63 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const Movie = require("../movieModel");
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+mongoose.connect("mongodb://localhost:27017/movies", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+});
+
+app.post("/movies", upload.single("image"), async (req, res) => {
+  const { title, year, genre, rating } = req.body;
+  const image = req.file.buffer;
+
+  const newMovie = new Movie({
+    title,
+    year,
+    genre,
+    rating,
+    image: image,
+  });
+  await newMovie.save();
+
+  res.send("Movie added successfully");
+});
+
+// route for getting all movies
+app.get("/getmovies", async (req, res) => {
+  try {
+    const movies = await Movie.find();
+    const moviesWithBase64Images = movies.map(movie => {
+      const base64Image = movie.image.toString('base64');
+      return {
+        ...movie._doc,
+        image: `data:image/jpg;base64,${base64Image}`,
+      };
+    });
+    res.json(moviesWithBase64Images);
+  } catch (err) {
+    res.status(500).send("Error fetching movies: " + err.message);
+  }
+});
+
+// Export your Express app as a serverless function
+module.exports = app;
 
